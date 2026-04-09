4e89b07a4788c3d3da78dd65c33d70e73e0d1ed1
JoaoManoelFontes <jmanufontesc@gmail.com>
2026-04-09 15:34:47 -0300

    feat: add drizzle and pg setup

--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,10 @@ const envSchema = z.object({
 	POSTGRES_USER: z.string().min(1),
 	POSTGRES_PASSWORD: z.string().min(1),
 	POSTGRES_DB: z.string().min(1),
+	POSTGRES_HOST: z.string().default("localhost"),
+	POSTGRES_PORT: z.coerce.number().default(5432),
 });
 
 export const env = envSchema.parse(process.env);
+
+export const DATABASE_URL = `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`;
