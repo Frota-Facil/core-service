75fa7be89a78213d610140c768fe34422a60e7ec
JoaoManoelFontes <jmanufontesc@gmail.com>
2026-04-25 14:36:31 -0300

    feat: add db:seed script

---
+++ src/drizzle/seed.ts
@@ -0,0 +1,40 @@
+import { db } from "@/drizzle/client";
+import { eq, and } from "drizzle-orm";
+import { users } from "@/domains/users/schema";
+import { USER_ROLES } from "@/domains/users/roles";
+import bcrypt from "bcryptjs";
+
+async function seed() {
+  const adminEmail = "admin@sif.com";
+  const adminCpf = "00000000000"
+
+  const [existing] = await db
+  .select()
+  .from(users)
+  .where(
+    and(
+      eq(users.email, adminEmail),
+      eq(users.cpf, adminCpf)
+    )
+  )
+  .limit(1);
+
+  if (existing) {
+    console.log("Admin já existe");
+    return;
+  }
+
+  const passwordHash = await bcrypt.hash("123456", 10);
+
+  await db.insert(users).values({
+    name: "AdminSIF",
+    email: adminEmail,
+    cpf: adminCpf,
+    passwordHash: passwordHash,
+    role: USER_ROLES[1],
+  });
+
+  console.log("Admin criado com sucesso");
+}
+
+seed();
